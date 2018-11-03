93d337406267f8a1309a1f1649ed1705d2290330
Michele Biondi <michelebiondi01@gmail.com>
2018-11-03 18:47:03 +0100

    Add functions to set and get configuration

--- test/index.test.js
+++ test/index.test.js
@@ -5,18 +5,44 @@ describe("Simple starting tests", () => {
         expect(slmfHttpConnector.isRunning()).toBe(false)
     })
     
-    test('slmf starts when told to do so', () => {
+    it('starts when told to do so', () => {
         slmfHttpConnector.start()
         expect(slmfHttpConnector.isRunning()).toBe(true)
     })
     
-    test('slmf stops when told to do so', () => {
+    it('stops when told to do so', () => {
         if(!slmfHttpConnector.isRunning())
             slmfHttpConnector.start()
         
         slmfHttpConnector.stop()
         expect(slmfHttpConnector.isRunning()).toBe(false)
     })
+
+    it('should return a configuration', () => {
+        const config = slmfHttpConnector.getConfiguration()
+        expect(config).toHaveProperty('url')
+        expect(config).toHaveProperty('port')
+        expect(config).toHaveProperty('maxSmlfMessages')
+        expect(config).toHaveProperty('accumulationPeriod')
+        expect(config).toHaveProperty('maxRetries')
+        expect(config).toHaveProperty('maxAccumulatedMessages')
+    })
+
+    it('should set the proper configuration', () => {
+        const config = {
+            url : 'http://example.com',
+            port : 80,
+            maxSmlfMessages : 512,
+            accumulationPeriod : 500,
+            maxRetries : 15,
+            maxAccumulatedMessages : 1024
+        }
+
+        slmfHttpConnector.setConfiguration(config)
+
+        expect(slmfHttpConnector.getConfiguration()).toEqual(config)
+    })
+
 })
 
 
