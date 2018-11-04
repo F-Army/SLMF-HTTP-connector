db609e94a41fb8bd65e95283dc9b26ee106f5f91
Michele Biondi <michelebiondi01@gmail.com>
2018-11-04 01:07:36 +0100

    Created validator for config

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+const Joi = require('joi')
+
+const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+const DEFAULT_MAX_RETRIES = 3
+
+const configSchema = Joi.object().keys({
+    url: Joi.string().regex(URL_REGEX).required(),
+    port: Joi.number().positive().min(0).max(65535).required(),
+    maxSlmfMessages: Joi.number().positive().min(0).max(1024).required(),
+    accumulationPeriod: Joi.number().positive().required(),
+    maxRetries: Joi.number().positive().default(DEFAULT_MAX_RETRIES),
+    maxAccumulatedMessages: Joi.number().min(Joi.ref('maxSlmfMessages')).default(Joi.ref('maxSlmfMessages'))
+})
+
 module.exports = class SlmfHttpConnector {
     constructor(config) {
-        this._config = config
+
+        const {error, value} = Joi.validate(config, configSchema)
+        if (!error)
+            this._config = value
+
         this._running = false
     }
 
     set config(config) {
-        this._config = config
+        const {error, value} = Joi.validate(config, configSchema)
+        if (!error)
+            this._config = value
     }
 
     get config () { return this._config }
