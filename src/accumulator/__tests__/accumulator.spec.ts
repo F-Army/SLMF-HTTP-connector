d220b39e29ea99dce6644a7f09476749cd14c72d
Michele Biondi <michelebiondi01@gmail.com>
2018-11-10 14:35:22 +0100

    Add ts-jest

--- src/accumulator/__tests__/accumulator.spec.ts
+++ src/accumulator/__tests__/accumulator.spec.ts
@@ -1,6 +1,6 @@
 "use strict";
 
-import Accumulator from "./accumulator";
+import Accumulator from "../accumulator";
 
 describe("Accumulator tests", () => {
     it("should accumulate data", () => {
