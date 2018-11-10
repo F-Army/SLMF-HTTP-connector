63932bf743a20c1e8feedf94b4be7f1db8bb5b89
Michele Biondi <michelebiondi01@gmail.com>
2018-11-10 19:47:18 +0100

    Add max messages check

--- src/slmfHttpConnector/slmfHttpConnector.ts
+++ src/slmfHttpConnector/slmfHttpConnector.ts
@@ -22,8 +22,15 @@ class SlmfHttpConnector {
         this.accumulator = new Accumulator(this.settings.maxAccumulatedMessages);
 
         this.loop = new ConnectorLoop( async () => {
-            if (this.accumulator.data.length > 0) {
-                const messages = copyArray(this.accumulator.data);
+
+            const shifts = this.settings.maxSlmfMessages <= this.accumulator.data.length ?
+                                this.settings.maxSlmfMessages : this.accumulator.data.length;
+
+            if (shifts > 0) {
+                const messages = [];
+                for (let i = 0; i < shifts; i++) {
+                    messages.push(this.accumulator.data.shift());
+                }
                 this.accumulator.clear();
                 await axios.post(this.settings.url, {data: messages});
             }
