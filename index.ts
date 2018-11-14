47a463946b33543fb442675fedba7c31a7ede49d
Michele Biondi <michelebiondi01@gmail.com>
2018-11-14 14:28:58 +0100

    Add typescript related exports

--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
 export { default as SlmfHttpConnector } from "./lib/SlmfHttpConnector";
 export * from "./lib/LocationMessage";
+
+// Typescript related export
+export * from "./lib/Accumulator";
+export * from "./lib/ConnectorLoop";
+export * from "./lib/ConnectorSettings";
