7fc7a61225395fd5ed828e2e8d6b44e01fb4db3f
Michele Biondi <michelebiondi01@gmail.com>
2018-11-14 14:32:26 +0100

    Reverted last commit

--- index.ts
+++ index.ts
@@ -1,7 +1,2 @@
 export { default as SlmfHttpConnector } from "./lib/SlmfHttpConnector";
 export * from "./lib/LocationMessage";
-
-// Typescript related export
-export * from "./lib/Accumulator";
-export * from "./lib/ConnectorLoop";
-export * from "./lib/ConnectorSettings";
